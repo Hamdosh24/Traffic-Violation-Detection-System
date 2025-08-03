2670c9a7c8252a95b399ba172bff080ba124f080
KareemKallash <kareemkallash18@gmail.com>
2025-08-04 00:10:30 +0300

    Update AppServiceProvider.php

--- BackEnd/External API/app/Providers/AppServiceProvider.php
+++ BackEnd/External API/app/Providers/AppServiceProvider.php
@@ -3,13 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use App\Models\PersonalAccessToken;
-use Laravel\Sanctum\Sanctum;
-use App\Models\Violation;
-use App\Observers\ViolationObserver;
-use App\Models\Accident;
-use App\Observers\AccidentObserver;
-
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -26,10 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        Sanctum::usePersonalAccessTokenModel(PersonalAccessToken::class);
-        Violation::observe(ViolationObserver::class); 
-        Accident::observe(AccidentObserver::class); 
-
-
+        //
     }
 }
