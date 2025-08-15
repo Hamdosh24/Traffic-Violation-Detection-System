1bf72c22cd9e055ee4659672903f17093df518d3
Beshr Alkeddeh <beshralkeddeh@gmail.com>
2025-08-15 09:29:29 +0300

    edit rtsp stream

--- camera_stream_simulation/1-Stream_RTSP.bat
+++ camera_stream_simulation/1-Stream_RTSP.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem ==== إعداد المسارات ====
-set VIDEO_PATH=C:\Users\Dell\Documents\GitHub\Traffic-Violation-Detection-System\camera_stream_simulation\sample.mp4
+set VIDEO_PATH=C:\Users\Dell\Documents\GitHub\Traffic-Violation-Detection-System\camera_stream_simulation\sample2.mp4
 set FFMPEG_PATH=C:\ffmpeg\bin\ffmpeg.exe
 set VLC_PATH=C:\Program Files\VideoLAN\VLC\vlc.exe
 set MEDIA_MTX_PATH=C:\mediamtx\mediamtx.exe
@@ -12,6 +12,8 @@ start /min "" "C:\mediamtx\mediamtx.exe"
 
 timeout /t 5 /nobreak
 
-"%FFMPEG_PATH%" -re -i "%VIDEO_PATH%" -c:v libx264 -preset ultrafast -tune zerolatency -g 30 -r 30 -c:a aac -f rtsp -rtsp_transport tcp %STREAM_URL%
+:loop
+"%FFMPEG_PATH%" -re -i "%VIDEO_PATH%" -c:v libx264 -preset ultrafast -tune zerolatency -g 30 -r 15 -c:a aac -f rtsp -rtsp_transport tcp %STREAM_URL%
+goto loop
 
 pause
