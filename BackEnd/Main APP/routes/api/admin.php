df7d6574abeed25f48494b7bc2f4581d5cd9a227
KareemKallash <kareemkallash18@gmail.com>
2025-08-18 08:33:51 +0300

    Update admin.php

--- BackEnd/Main APP/routes/api/admin.php
+++ BackEnd/Main APP/routes/api/admin.php
@@ -3,10 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Api\AccidentController;
 use App\Http\Controllers\Api\PassingCarController;
-Route::get('/accidents/stream', action: [AccidentController::class, 'streamNewAccidents']);
 
 // Accident & Search Management (For Employees & Managers)
 Route::middleware(['auth:sanctum', 'token.expires', 'employee'])->group(function () {
+    Route::get('/accidents/stream', action: [AccidentController::class, 'streamNewAccidents']);
     Route::get('/accidents/all', [AccidentController::class, 'indexAll']);
     Route::patch('/accidents/{accident}/viewed', [AccidentController::class, 'markAsViewed']); //
     Route::get('/passing-cars/search/{plate_num}', [PassingCarController::class, 'searchByPlate']); //
