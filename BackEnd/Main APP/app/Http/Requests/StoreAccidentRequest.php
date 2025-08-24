e6a7de9a949e7c3abe3863825ec403bde6af3ccc
KareemKallash <kareemkallash18@gmail.com>
2025-08-24 15:35:37 +0300

    refactor(Validation): Use FormRequests for Accident & PassingCar controllers
    - Extracted validation logic from the store() methods of AccidentController and PassingCarController into dedicated FormRequest classes (StoreAccidentRequest & StorePassingCarRequest).
    
    - This change cleans up the controllers, centralizes validation rules, and ensures a consistent and robust validation approach across the entire application.

---
+++ BackEnd/Main APP/app/Http/Requests/StoreAccidentRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreAccidentRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'camera_id' => 'required|integer|exists:cameras,camera_id',
+            'timestamp' => 'required|date',
+        ];
+    }
+}
