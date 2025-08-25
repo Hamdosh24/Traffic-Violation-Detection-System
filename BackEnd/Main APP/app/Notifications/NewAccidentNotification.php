8018ebd290c7fe3134b270403bf87438f23f74f9
KareemKallash <kareemkallash18@gmail.com>
2025-08-25 12:33:00 +0300

    update accident controller

--- BackEnd/Main APP/app/Notifications/NewAccidentNotification.php
+++ BackEnd/Main APP/app/Notifications/NewAccidentNotification.php
@@ -22,7 +22,6 @@ class NewAccidentNotification extends Notification implements ShouldQueue
 
     public function via(object $notifiable): array
     {
-        // <-- التعديل: تحديد وجهة الإشعار إلى قاعدة البيانات
         return ['database'];
     }
 
