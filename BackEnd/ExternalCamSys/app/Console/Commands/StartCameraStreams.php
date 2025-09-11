80bb104b2ed6c251881bc2e6a32f51de849183ca
Beshr Alkeddeh <beshralkeddeh@gmail.com>
2025-09-11 19:25:21 +0300

    modify ERD

--- BackEnd/ExternalCamSys/app/Console/Commands/StartCameraStreams.php
+++ BackEnd/ExternalCamSys/app/Console/Commands/StartCameraStreams.php
@@ -9,22 +9,19 @@ class StartCameraStreams extends Command
     protected $signature = 'streams:start';
     protected $description = 'تشغيل ملفات الباتش للبث المباشر';
 
-    // app/Console/Commands/StartStreams.php
-
     public function handle()
     {
-        // $batFile = 'C:\\Users\\Dell\\Documents\\GitHub\\Traffic-Violation-Detection-System\\camera_stream_simulation\\1-Stream_RTSP.bat';
         $batFile = base_path('storage/camera_stream_simulation/1-Stream_RTSP.bat');
 
         if (!file_exists($batFile)) {
-            $this->error("❌ الملف غير موجود: $batFile");
+            $this->error("الملف غير موجود: $batFile");
             return;
         }
 
         // تشغيل الباتش كبروسس مستقل
         pclose(popen('start "" "' . $batFile . '"', 'r'));
 
-        $this->info("✅ تم تشغيل ملف الباتش كبروسس مستقل.");
+        $this->info("تم تشغيل ملف الباتش كبروسس مستقل.");
     }
 
 }
